a1a1c081a2b9f321b01c51ed692deaee3791fef1
Logan Ramos <lramos15@gmail.com>
2025-01-21 07:24:00 -0700

    Fix telemetry properties (#123)
    @karthiknadig Joh made a nice eslint rule for this which may make be
    useful to adopt to avoid these cases
    
    
    https://github.com/microsoft/vscode-copilot/blob/main/.eslintplugin/no-bad-gdpr-comment.ts

--- src/common/telemetry/constants.ts
+++ src/common/telemetry/constants.ts
@@ -1,31 +1,31 @@
 export enum EventNames {
-    EXTENSION_ACTIVATION_DURATION = 'EXTENSION.ACTIVATION_DURATION',
-    EXTENSION_MANAGER_REGISTRATION_DURATION = 'EXTENSION.MANAGER_REGISTRATION_DURATION',
+    EXTENSION_ACTIVATION_DURATION = "EXTENSION.ACTIVATION_DURATION",
+    EXTENSION_MANAGER_REGISTRATION_DURATION = "EXTENSION.MANAGER_REGISTRATION_DURATION",
 
-    ENVIRONMENT_MANAGER_REGISTERED = 'ENVIRONMENT_MANAGER.REGISTERED',
-    PACKAGE_MANAGER_REGISTERED = 'PACKAGE_MANAGER.REGISTERED',
+    ENVIRONMENT_MANAGER_REGISTERED = "ENVIRONMENT_MANAGER.REGISTERED",
+    PACKAGE_MANAGER_REGISTERED = "PACKAGE_MANAGER.REGISTERED",
 
-    VENV_USING_UV = 'VENV.USING_UV',
+    VENV_USING_UV = "VENV.USING_UV",
 }
 
 // Map all events to their properties
 export interface IEventNamePropertyMapping {
     /* __GDPR__
        "extension_activation_duration": {
-           "duration" : { "classification": "SystemMetaData", "purpose": "FeatureInsight", "owner": "karthiknadig" },
+           "duration" : { "classification": "SystemMetaData", "purpose": "FeatureInsight", "owner": "karthiknadig" }
        }
     */
     [EventNames.EXTENSION_ACTIVATION_DURATION]: never | undefined;
     /* __GDPR__
        "extension_manager_registration_duration": {
-           "duration" : { "classification": "SystemMetaData", "purpose": "FeatureInsight", "owner": "karthiknadig" },
+           "duration" : { "classification": "SystemMetaData", "purpose": "FeatureInsight", "owner": "karthiknadig" }
        }
     */
     [EventNames.EXTENSION_MANAGER_REGISTRATION_DURATION]: never | undefined;
 
     /* __GDPR__
         "environment_manager_registered": {
-            "managerId" : { "classification": "SystemMetaData", "purpose": "FeatureInsight", "owner": "karthiknadig" },
+            "managerId" : { "classification": "SystemMetaData", "purpose": "FeatureInsight", "owner": "karthiknadig" }
         }
     */
     [EventNames.ENVIRONMENT_MANAGER_REGISTERED]: {
@@ -34,7 +34,7 @@ export interface IEventNamePropertyMapping {
 
     /* __GDPR__
         "package_manager_registered": {
-            "managerId" : { "classification": "SystemMetaData", "purpose": "FeatureInsight", "owner": "karthiknadig" },
+            "managerId" : { "classification": "SystemMetaData", "purpose": "FeatureInsight", "owner": "karthiknadig" }
         }
     */
     [EventNames.PACKAGE_MANAGER_REGISTERED]: {
