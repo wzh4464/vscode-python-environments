e580490d156e6be81ca6011603f018ce621035bf
Karthik Nadig <kanadig@microsoft.com>
2025-02-10 08:03:13 -0800

    fix: wrap the experimental shell type API with try-catch (#175)
    fixes https://github.com/microsoft/vscode-python-environments/issues/159
    
    We need to wrap the API in a try-catch block to handle any potential
    errors that might occur as it is currently being worked on.

--- src/features/common/shellDetector.ts
+++ src/features/common/shellDetector.ts
@@ -125,28 +125,33 @@ function identifyShellFromSettings(): TerminalShellType {
 }
 
 function fromShellTypeApi(terminal: Terminal): TerminalShellType {
-    switch (terminal.state.shellType) {
-        case TerminalShellTypeVscode.Sh:
-        case TerminalShellTypeVscode.Bash:
-            return TerminalShellType.bash;
-        case TerminalShellTypeVscode.Fish:
-            return TerminalShellType.fish;
-        case TerminalShellTypeVscode.Csh:
-            return TerminalShellType.cshell;
-        case TerminalShellTypeVscode.Ksh:
-            return TerminalShellType.ksh;
-        case TerminalShellTypeVscode.Zsh:
-            return TerminalShellType.zsh;
-        case TerminalShellTypeVscode.CommandPrompt:
-            return TerminalShellType.commandPrompt;
-        case TerminalShellTypeVscode.GitBash:
-            return TerminalShellType.gitbash;
-        case TerminalShellTypeVscode.PowerShell:
-            return TerminalShellType.powershellCore;
-        case TerminalShellTypeVscode.NuShell:
-            return TerminalShellType.nushell;
-        default:
-            return TerminalShellType.unknown;
+    try {
+        switch (terminal.state.shellType) {
+            case TerminalShellTypeVscode.Sh:
+            case TerminalShellTypeVscode.Bash:
+                return TerminalShellType.bash;
+            case TerminalShellTypeVscode.Fish:
+                return TerminalShellType.fish;
+            case TerminalShellTypeVscode.Csh:
+                return TerminalShellType.cshell;
+            case TerminalShellTypeVscode.Ksh:
+                return TerminalShellType.ksh;
+            case TerminalShellTypeVscode.Zsh:
+                return TerminalShellType.zsh;
+            case TerminalShellTypeVscode.CommandPrompt:
+                return TerminalShellType.commandPrompt;
+            case TerminalShellTypeVscode.GitBash:
+                return TerminalShellType.gitbash;
+            case TerminalShellTypeVscode.PowerShell:
+                return TerminalShellType.powershellCore;
+            case TerminalShellTypeVscode.NuShell:
+                return TerminalShellType.nushell;
+            default:
+                return TerminalShellType.unknown;
+        }
+    } catch {
+        // If the API is not available, return unknown
+        return TerminalShellType.unknown;
     }
 }
 
